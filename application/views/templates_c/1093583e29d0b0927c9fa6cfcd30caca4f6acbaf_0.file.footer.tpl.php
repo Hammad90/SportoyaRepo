2ff624e565894c330413eb71b777bc50659fcaf7
Mohamed Hammad <moh.hammad.90@gmail.com>
2015-12-22 02:45:06 +0200

    Adding language file label_lang.php

---
+++ application/views/templates_c/1093583e29d0b0927c9fa6cfcd30caca4f6acbaf_0.file.footer.tpl.php
@@ -0,0 +1,31 @@
+<?php /* Smarty version 3.1.27, created on 2015-12-22 02:44:04
+         compiled from "application/views/templates/footer.tpl" */ ?>
+<?php
+/*%%SmartyHeaderCode:54907939856789cd44f7185_23459837%%*/
+if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    '1093583e29d0b0927c9fa6cfcd30caca4f6acbaf' => 
+    array (
+      0 => 'application/views/templates/footer.tpl',
+      1 => 1254146842,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '54907939856789cd44f7185_23459837',
+  'has_nocache_code' => false,
+  'version' => '3.1.27',
+  'unifunc' => 'content_56789cd4503ec2_52949405',
+),false);
+/*/%%SmartyHeaderCode%%*/
+if ($_valid && !is_callable('content_56789cd4503ec2_52949405')) {
+function content_56789cd4503ec2_52949405 ($_smarty_tpl) {
+
+$_smarty_tpl->properties['nocache_hash'] = '54907939856789cd44f7185_23459837';
+?>
+</body>
+</html>
+<?php }
+}
+?>
